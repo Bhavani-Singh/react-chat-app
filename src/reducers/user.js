faa97ddf842f13b523610553555d64a420590c9b
BhavaniSingh <bhavanisinghrajput24@gmail.com>
2023-07-14 01:50:49 +0530

    push code

---
+++ src/reducers/user.js
@@ -0,0 +1,8 @@
+export default function user(state = {}) {
+  return {
+    id: 1,
+    name: "Bhavani Singh",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjzJjPa-3jdL6XAI0yqXBY8VzK_p5h0yQIkQ&usqp=CAU",
+  };
+}
